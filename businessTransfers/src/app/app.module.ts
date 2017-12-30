ccb76b06069cc96a0312abb6dcdb4d3075c0e480
jaggia <anadi.jaggia@gmail.com>
2017-12-30 16:57:57 -0500

    collapse fix

--- businessTransfers/src/app/app.module.ts
+++ businessTransfers/src/app/app.module.ts
@@ -8,11 +8,10 @@ import { SideBarComponent } from './side-bar/side-bar.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { HomeSlideComponent } from './home-slide/home-slide.component';
 import { AboutUsComponent } from './about-us/about-us.component';
-import { BTSOverviewComponent } from './about-us/btsoverview/btsoverview.component';
 import { WhoWeServeComponent } from './about-us/who-we-serve/who-we-serve.component';
 import { Routes, RouterModule } from '@angular/router';
 import { TransactionsComponent } from './about-us/transactions/transactions.component';
-import { routerTransition } from 'router.animations';
+// import { routerTransition } from 'router.animations';
 import { WhyHireUsComponent } from './about-us/why-hire-us/why-hire-us.component';
 import { TestimonialsComponent } from './testimonials/testimonials.component';
 import { InTheNewsComponent } from './in-the-news/in-the-news.component';
@@ -29,7 +28,6 @@ import { AgmCoreModule } from '@agm/core';
 const appRoutes : Routes = [
   {path : '', component: HomeSlideComponent},
   {path : 'about-us', component: AboutUsComponent},
-  {path : 'btsoverview', component: BTSOverviewComponent},
   {path : 'who-we-serve', component: WhoWeServeComponent},
   {path : 'transactions', component: TransactionsComponent},
   {path : 'why-hire-us', component: WhyHireUsComponent},
@@ -41,7 +39,8 @@ const appRoutes : Routes = [
   {path : 'phase2', component: Phase2Component},
   {path : 'phase3', component: Phase3Component},
   {path : 'advisors', component: AdvisorsComponent},
-  {path : 'contactus', component: ContactusComponent},
+  {path : 'contact-us', component: ContactusComponent},
+  {path : '**', component: HomeSlideComponent}
 ];
 
 
@@ -51,7 +50,6 @@ const appRoutes : Routes = [
     SideBarComponent,
     HomeSlideComponent,
     AboutUsComponent,
-    BTSOverviewComponent,
     WhoWeServeComponent,
     TransactionsComponent,
     WhyHireUsComponent,
